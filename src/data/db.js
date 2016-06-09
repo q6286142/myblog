812fb7ebac11fcf4ec2e21e272680721797875da
q6286142 <q6286142@163.com>
2016-06-09 16:20:55 +0800

    change db.js mongo var

--- src/data/db.js
+++ src/data/db.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 const uri = 'mongodb://qjy:6286142jia!@localhost:27017/blog';
 
-let mongo = mongoose.createConnection(uri)
+mongoose.createConnection(uri)
+
+let mongo = mongoose.connection;
 
 mongo.on('error', function(err){
     if(err){
